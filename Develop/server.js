6a82c9692b52bf4c65ee2135fad05cec0f3f6b08
bcebel <bcebel@gmail.com>
2023-03-24 23:55:51 -0400

    updates

--- Develop/server.js
+++ Develop/server.js
@@ -1,56 +1,75 @@
-const express = require("express");
-const path = require("path");
 const PORT = process.env.PORT || 3001;
-var randomid = require("./helpers/fsUtils");
+const fs = require("fs");
+const path = require("path");
+
+const express = require("express");
 const app = express();
 
-app.use(express.json());
+const allNotes = require("./db/db.json");
+
 app.use(express.urlencoded({ extended: true }));
-//app.use("/api", api);
+app.use(express.json());
 app.use(express.static("public"));
 
-app.get("/notes", (req, res) =>
-  res.sendFile(path.join(__dirname, "/public/notes.html"))
-);
+app.get("/api/notes", (req, res) => {
+  res.json(allNotes.slice(1));
+});
 
-app.get("/api/notes", (req, res) =>
-  res.json(JSON.parse(readFromFile("./db/db.json")))
-);
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+function createNewNote(body, notesArray) {
+  const newNote = body;
+  if (!Array.isArray(notesArray)) notesArray = [];
+
+  if (notesArray.length === 0) notesArray.push(0);
+
+  body.id = notesArray[0];
+  notesArray[0]++;
+
+  notesArray.push(newNote);
+  fs.writeFileSync(
+    path.join(__dirname, "./db/db.json"),
+    JSON.stringify(notesArray, null, 2)
+  );
+  return newNote;
+}
 
 app.post("/api/notes", (req, res) => {
-  let jsonFilePath = path.join(__dirname, "/db/db.json");
-  let newNote = req.body;
-
-  // This allows the test note to be the original note.
-  let highestId = 99;
-  // This loops through the array and finds the highest ID.
-  for (let i = 0; i < database.length; i++) {
-    let individualNote = database[i];
-
-    if (individualNote.id > highestId) {
-      // highestId will always be the highest numbered id in the notesArray.
-      highestId = individualNote.id;
-    }
-  }
-  // This assigns an ID to the newNote.
-  newNote.id = highestId + 1;
-  // We push it to db.json.
-  database.push(newNote);
-
-  // Write the db.json file again.
-  fs.writeFile(jsonFilePath, JSON.stringify(database), function (err) {
-    if (err) {
-      return console.log(err);
-    }
-    console.log("Your note was saved!");
-  });
-  // Gives back the response, which is the user's new note.
+  const newNote = createNewNote(req.body, allNotes);
   res.json(newNote);
 });
 
-app.get("*", (req, res) =>
-  res.sendFile(path.join(__dirname, "/public/index.html"))
-);
-app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
-);
+function deleteNote(id, notesArray) {
+  for (let i = 0; i < notesArray.length; i++) {
+    let note = notesArray[i];
+
+    if (note.id == id) {
+      notesArray.splice(i, 1);
+      fs.writeFileSync(
+        path.join(__dirname, "./db/db.json"),
+        JSON.stringify(notesArray, null, 2)
+      );
+
+      break;
+    }
+  }
+}
+
+app.delete("/api/notes/:id", (req, res) => {
+  deleteNote(req.params.id, allNotes);
+  res.json(true);
+});
+
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
+});
