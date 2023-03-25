1157d1509667f9ca1227d4ff79b0efdb8409c460
bcebel <bcebel@gmail.com>
2023-03-25 00:12:30 -0400

    readme

--- Develop/server.js
+++ Develop/server.js
@@ -1,16 +1,12 @@
 const PORT = process.env.PORT || 3001;
 const fs = require("fs");
 const path = require("path");
-
 const express = require("express");
 const app = express();
-
 const allNotes = require("./db/db.json");
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-
 app.get("/api/notes", (req, res) => {
   res.json(allNotes.slice(1));
 });
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Note Taker Starter Code
+
+## Description
+
+This is a Note Taker app.  
+It is deployed on Heroku here:
+https://whispering-depths-77295.herokuapp.com
+It uses get and put routes.
+
+<img src="./screenshot.png" alt="Alt text" title="screenshot">
+
+## License
+
+MIT License, see repository for details.
